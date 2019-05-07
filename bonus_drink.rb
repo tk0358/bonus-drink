75245d970830eb2f7b29206a7512bedcb0a3e1e0
tk0358 <tk0358a@yahoo.co.jp>
2019-05-08 02:54:00 +0900

    kaitou

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    num = amount
+    drink_ary = [num]
+    while num >= 3
+      drink_ary << num / 3
+      num = num / 3 + num % 3
+    end
+    drink_ary.sum
   end
 end
